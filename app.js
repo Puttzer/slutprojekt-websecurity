d15da0eacd6959fcd9bcf2de497b82d336d4dc4a
Puttzer <carragher@live.se>
2020-02-04 18:00:20 +0100

    Added comments

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ const app = express();
 PORT = process.env.PORT || 4000;
 
 
-
+// in
 const Products = require("./routes/products");
 // const ordersRoutes = require("./routes/orders");
 const auth = require('./routes/register');
 const registerRoute = require('./routes/register');
 
-
 app.use(express.static("public"));
 app.use(express.json());
 
+
+// out
 app.use("/", registerRoute);
 app.use("/", auth);
 app.use("/", Products);
